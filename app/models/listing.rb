4249bc66fb5380f9ae4cebc5fc00fa05e19f010d
Kenneth Chacon Villalobos <kchacon@cr.ibm.com>
2015-10-26 14:18:22 -0600

    Lecture 21: Configuration on Dropbox and LocalHost

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,14 @@
 class Listing < ActiveRecord::Base
-  has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "default.jpg",
-                    :storage => :dropbox,
-                    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
   
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  if Rails.env.development?
+      has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "default.jpg"
+      validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  else
+      has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "default.jpg",
+                        :storage => :dropbox,
+                        :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+                        :path => ":style/:id_:filename"
+  
+      validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  end
 end
