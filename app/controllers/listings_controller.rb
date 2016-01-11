590fb99077dfb89d4b4837a2f0a9c316e2857002
Kenneth Chacon Villalobos <kchacon@cr.ibm.com>
2016-01-11 10:34:31 -0600

    Lecture 28: Set User Permissions and conditional on links

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,7 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_filter :check_user, only: [:edit, :update, :destroy]
 
   # GET /listings
   # GET /listings.json
@@ -72,4 +74,11 @@ class ListingsController < ApplicationController
     def listing_params
       params.require(:listing).permit(:name, :description, :price, :image)
     end
+    
+    def check_user
+      if current_user != @listing.user 
+        redirect_to root_url, alert: "You are not an owner for this listing!"
+      end
+    end
+    
 end
